b17c95f921805b667a476e612485b0e21a98a713
Carlos Almendros <77893059+carlosuidev@users.noreply.github.com>
2024-05-27 22:33:59 +0200

    fix(App.jsx): Fixing button name

--- src/components/EditPlayerForm.jsx
+++ src/components/EditPlayerForm.jsx
@@ -160,7 +160,7 @@ export const EditPlayerForm = (props) => {
             <button
               type='submit'
               className='duration-300 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600'>
-              Saves changes
+              Save changes
             </button>
           </div>
         </form>
